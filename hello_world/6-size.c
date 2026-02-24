a1adb224f6807b1a33d8a6fae73e61a761243de3
Ghaida <alsabtighaida@gmail.com>
2026-02-24 19:19:20 +0000

    Add task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,10 +11,10 @@ int i;
 long int ln;
 long l;
 float f;
-printf("Size of a char: %d byte(s)", (int)sizeof(c));
-printf("Size of an int: %d byte(s)", (int)sizeof(i));
-printf("Size of a long int: %d byte(s)", (int)sizeof(ln));
-printf("Size of a long long: %d byte(s)", (int)sizeof(l));
+printf("Size of a char: %d byte(s)\n", (int)sizeof(c));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(i));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(ln));
+printf("Size of a long long: %d byte(s)\n", (int)sizeof(l));
 printf("Size of a float: %d byte(s)", (int)sizeof(f));
 return (0);
 }
