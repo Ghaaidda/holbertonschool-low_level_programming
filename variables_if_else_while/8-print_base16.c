9c87089be8da0dfb78829a4238b0c69d6381f409
Ghaida <alsabtighaida@gmail.com>
2026-02-25 19:05:58 +0000

    Add task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i = 'a';
+for (i = 'a'; i <= 'f'; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
