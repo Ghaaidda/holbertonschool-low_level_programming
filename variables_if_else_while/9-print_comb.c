c34cbf873e6e10ee8eeac8db6a660463c588911b
Ghaida <alsabtighaida@gmail.com>
2026-02-25 19:27:51 +0000

    Add task 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,13 +9,11 @@ int main(void)
 int i = '0';
 for (i = '0'; i <= '9'; i++)
 {
-if (i == '9')
+putchar((char)i);
+if (i != '9')
 {
-putchar((char)i);
-return (0);
-}
-putchar((char)i);
 putchar(',');
+}
 putchar(' ');
 }
 return (0);
