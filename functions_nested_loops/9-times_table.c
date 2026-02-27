c5f92efbdffe85805589065e7cb40b199182eb9e
alsabtighaida@gmail.com <Ghaida>
2026-02-27 16:30:32 +0000

    Add task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,15 +6,19 @@
  */
 void times_table(void)
 {
-int i = 0;
-int j = 0;
+int i = 0, j, res;
 while (i < 10)
 {
+j = 0;
 while (j < 10)
 {
-_putchar(j * i);
-_putchar(',');
-_putchar(' ');
+res = i * j;
+if (res > 9)
+{
+_putchar((res / 10) + '0');
+_putchar((res % 10) + '0');
+}
+_putchar(res + '0');
 j++;
 }
 _putchar('\n');
