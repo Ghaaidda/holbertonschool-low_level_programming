5bb9907df6f0cd2a7a00aa09f6393c4cd1fb4596
ghaida <alsabtighaida@gmail>
2026-02-27 11:35:56 +0000

    Add task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of an int
+ *@n: The int to check
+ *
+ * Return: absolute value.
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
