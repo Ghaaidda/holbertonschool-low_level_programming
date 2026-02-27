9a8eb039c0519c8d323fde8631da05d62d18b14c
alsabtighaida@gmail.com <Ghaida>
2026-02-27 15:32:15 +0000

    Add task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+int h = 0;
+int m = 0;
+for (h = 0; h < 24; h++)
+{
+if (h < 10)
+{
+_putchar('0');
+}
+_putchar(h);
+for (m = 0; m < 60; m++)
+{
+_putchar(':');
+_putchar(m);
+}
+_putchar('\n');
+}
+}
