17d1c71d3164eac6df98dc030821edf47d4011f5
ghaida <alsabtighaida@gmail.com>
2026-02-28 12:57:52 +0000

    Made it shorter

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,15 +8,17 @@
  */
 void print_to_98(int n)
 {
-while (n != 98){
+while (n != 98)
+{
 printf("%d, ", n);
-if (n > 98){
+if (n > 98)
+{
 n--;
 }
 else
 {
 n++;
 }
-printf("98\n");
 }
+printf("98\n");
 }
