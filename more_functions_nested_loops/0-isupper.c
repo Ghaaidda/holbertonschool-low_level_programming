ab33f88d2d53319353bca603f39fde1caa814e5c
Ghaida <alsabtighaida@gmail.com>
2026-03-01 18:25:51 +0000

    Add task 0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - checks if character is uppercase
+ * @c: character to check
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+if (c > 64 && c < 91)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
