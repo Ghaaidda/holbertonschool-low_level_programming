8dcbbe189059dada8a12f15a746e9ce9d77a97e7
Ghaida <alsabtighaida@gmail.com>
2026-03-01 19:56:55 +0000

    Add task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: square size
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+int i;
+int j;
+if (size > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
