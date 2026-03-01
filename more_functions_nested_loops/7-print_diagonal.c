df716e177f56baa6d1d8af127ea8e5a5c4aff896
Ghaida <alsabtighaida@gmail.com>
2026-03-01 19:20:10 +0000

    Add task 7

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal line
+ * @n: number of backslashs (line length)
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar('\');
+}
+}
+_putchar('\n');
+}
