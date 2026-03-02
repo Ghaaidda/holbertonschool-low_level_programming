23aadf2a8a2db515850f8857d0fd2b41604280e5
ghaida <alsabtighaida@gmail.com>
2026-03-02 18:39:37 +0000

    Add task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: number of hash signs
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+for (j = n; j < 0; j--)
+{
+_putchar(' ');
+}
+_putchar('#');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
