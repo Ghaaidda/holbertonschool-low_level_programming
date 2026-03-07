9bd8cb8cec8edd3255e4127502f70843ec98657f
Ghaida <alsabtighaida@gmail.com>
2026-03-07 12:16:45 +0300

    Add task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *  puts2 - prints every other character of a string
+ * @str: the string
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str += 2;
+}
+_putchar('\n');
+}
