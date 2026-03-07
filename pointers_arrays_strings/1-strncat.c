ba145e78c74248da6ef86e1c4ad3f08bae043853
Ghaida <alsabtighaida@gmail.com>
2026-03-07 16:18:28 +0300

    Add task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates the strings
+ * @dest: A pointer to the string to be concatenated upon
+ * @src: The source string to be appended to @dest
+ * @n: number of element to concatenate from src
+ *
+ * Return: A pointer to the destination string @dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+char *ptr1 = dest, *ptr2 = src, *ptr3 = dest;
+int i = 0, j;
+while (*ptr1)
+{
+ptr1++;
+i++;
+}
+for (j = 0; j < n; j++)
+{
+dest[i++] = src[j++];
+ptr2++;
+}
+return (ptr3);
+}
