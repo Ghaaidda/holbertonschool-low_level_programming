7aea739f02323cfae3e35390463c845a68e706c8
Ghaida <alsabtighaida@gmail.com>
2026-03-13 13:54:11 +0000

    Add task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - counts length of a prefix substring.
+ * @s: the string.
+ * @accept: the substring.
+ *
+ * Return: prefix lenght.
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+unsigned int len = 0;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*s == accept[i])
+{
+len++;
+break;
+}
+else if (accept[i + 1] == '\0')
+{
+return (len);
+}
+}
+s++;
+}
