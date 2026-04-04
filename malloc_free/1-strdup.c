cb426b778709208b40fa2a7879283d68cb6c250e
Ghaida <alsabtighaida@gmail.com>
2026-04-04 13:55:33 +0000

    Add task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup -  returns a pointer to a new string
+ * @str: the a string
+ *
+ * Return: a pointer to the string or null if allocation fails
+ */
+char *_strdup(char *str)
+{
+char *pts;
+unsigned int i, len = 0;
+for (i = 0; str[i]; i++)
+len++;
+pts = malloc(sizeof(char) * (len + 1));
+if (!str || !pts)
+return (NULL);
+for (i = 0; str[i]; i++)
+pts[i] = str[i];
+pts[len] = '\0';
+return (pts);
+}
