49e6e2afc7ab8175b1ba482bf94b8c34bba3b38b
Ghaida <alsabtighaida@gmail.com>
2026-04-04 13:37:22 +0000

    Add task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,11 +10,11 @@
 char *create_array(unsigned int size, char c)
 {
 char *str;
-int i;
+unsigned int i;
 str = malloc(sizeof(char) * size);
 if (str == NULL)
 return (NULL);
-for (i = 0; i < size; i++)	
+for (i = 0; i < size; i++)
 str[i] = c;
 return (str);
 }
