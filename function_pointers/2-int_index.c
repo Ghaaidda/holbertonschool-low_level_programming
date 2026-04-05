eee0179b0b9498414a3b3dc10bce094db17210bc
Ghaida <alsabtighaida@gmail.com>
2026-04-05 13:37:54 +0000

    Add task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,8 +13,8 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int index;
-if (array != NULL)
-{	
+if (array != NULL && cmp != NULL)
+{
 for (index = 0; index < size; index++)
 {
 if (cmp(array[index]))
