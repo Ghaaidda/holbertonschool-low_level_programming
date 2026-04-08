d757323b5008751be2cf883a817a4ac61027953a
Ghaida <alsabtighaida@gmail.com>
2026-04-08 17:42:06 +0000

    Fixed

--- function_pointers/get_op_func.c
+++ function_pointers/get_op_func.c
@@ -1,12 +1,13 @@
 #include "calc.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * get_op_func - returns a pointer to a function that matches s
  * @s: operator sign
  *
  * Return: pointer to int return type function
  */
-int *get_op_func(char *s)
+int (*get_op_func(char *s))(int, int)
 {	
 op_t ops[] = {
         {"+", op_add},
@@ -16,11 +17,12 @@ op_t ops[] = {
         {"%", op_mod},
         {NULL, NULL}
     };
-int i;
+int i = 0 ;
 while (i < 6)
 {	
 if (strcmp(s, ops[i].op) == 0)
 return (ops[i].f);
 i++;
+return (NULL);
 }
 }
--- function_pointers/main.c
+++ function_pointers/main.c
@@ -1,8 +1,18 @@
 #include "calc.h"
 #include <stdio.h>
-int main(void)
+int main(int argc, char *argv[])
 {
-int res;
-scanf("%d", &res);
-printf("%d",get_op_func(res));
+int num1, num2, result;
+int (*fptr)(int, int);
+char *op;
+// 1. use atoi to convert string to int
+num1 = atoi(argv[0]);
+num2 = atoi(argv[2]);
+op = argv[1];
+// 2. get desired function
+fptr = get_ops_func(op);
+// 3. Save results and print it to user
+result = fptr(num1, num2);
+printf("%d", result);
+return (0);
 }
