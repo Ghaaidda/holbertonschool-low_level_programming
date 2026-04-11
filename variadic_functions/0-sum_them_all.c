ce81fe8c1d920cd44f66135cc4e34441a7d6c0c6
Ghaida <alsabtighaida@.com>
2026-04-11 15:20:34 +0000

    Add task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - sums all parameters
+ * @n:first argument
+ *
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int i, result;
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+result += va_arg(args, int);
+return (result);
+}
