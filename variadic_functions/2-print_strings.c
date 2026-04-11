801b331efd48f074c69f762cc320d88254bc047c
Ghaida <alsabtighaida@.com>
2026-04-11 16:18:47 +0000

    Add task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ str = va_arg(args, char *);
 if (str == NULL)
 printf("nil");
 else
-printf("%s", va_arg(args, char *));
+printf("%s", str);
 if (separator != NULL &&  i != (n - 1))
 printf("%s", separator);
 }
