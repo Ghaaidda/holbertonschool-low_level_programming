b3572ade3819f8c45db30fa89aa532b7e0e8faba
Ghaida <alsabtighaida@.com>
2026-04-11 12:39:13 +0000

    Add task 3

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -8,15 +8,15 @@
  * Return: pointer to int return type function
  */
 int (*get_op_func(char *s))(int, int)
-{	
+{
 op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
 int i = 0;
 while (i < 5)
 {
