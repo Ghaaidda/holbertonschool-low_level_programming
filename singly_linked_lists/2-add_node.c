7a7dbc44da9dfc17c24dd23d57b0111ba2f276ad
Ghaida <alsabtighaida@gmail.com>
2026-04-25 09:46:58 +0000

    Add task 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * add_node - add new node to SLL
+ * @head: new head
+ * @str: new head string data
+ *
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node->str = strdub(str);
+	new_node-> next = head->next;
+
+	return (new_node);
+}
