e764f0741019d5d41b6d2bf5e86bb1f953322dc2
Ghaida <alsabtighaida@gmail.com>
2026-04-25 09:57:17 +0000

    Add task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
-	new_node->str = strdub(str);
+	new_node->str = strdup(str);
 	new_node-> next = head->next;
 
 	return (new_node);
