8217023179d9605c16334b0ed490d725535eff62
Ghaida <alsabtighaida@gmail.com>
2026-04-25 10:18:07 +0000

    Add task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,21 +1,48 @@
 #include "lists.h"
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 /**
- * add_node - add new node to SLL
- * @head: new head
- * @str: new head string data
- *
- * Return: address of new element
+ * _strlen - gets length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * add_node - add new nodes to the list
+ * @head: current place in the list
+ * @str: string to add to the head
+ * Return: pointer to current position in list
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = NULL;
-	
-	new_node = malloc(sizeof(list_t));
+	int i, len;
+	char *content;
+	list_t *new;
 
-	new_node->str = strdup(str);
-	new_node->next = head->next;
-
-	return (new_node);
+	if (str == NULL || head == NULL)
+		return (NULL);
+	len = _strlen(str);
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }
