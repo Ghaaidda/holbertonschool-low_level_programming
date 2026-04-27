2968d6e3ef72a87c46a58b1f219500377a364eed
Ghaida <alsabtighaida@gmail.com>
2026-04-27 04:10:36 +0000

    Add task 0

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of hash table
+ *
+ * Return: pointer to new hash table or NULL if failed
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_ht = (hash_table_t)malloc(size);
+	return (new_ht);
+}
