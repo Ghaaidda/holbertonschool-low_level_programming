f00e19fcd916b6e665d03487ce24aea374256e1a
Ghaida <alsabtighaida@gmail.com>
2026-04-28 03:39:38 +0000

    Add task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,7 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_ht == NULL)
 		return (NULL);
 
-	hash_node_t **array = (hash_node_t *)malloc(sizeof(hash_node_t) * size);
+	hash_node_t **array = (hash_node_t *)malloc(sizeof(hash_node_t *) * size);
+
+	if (array == NULL)
+	{
+		free(new_ht);
+		return (NULL);
+	}
 
 	new_ht->size = size;
 	new_ht->array = array;
