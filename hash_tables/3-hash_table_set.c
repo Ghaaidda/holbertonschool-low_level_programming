a0361a535bace5e5062fd7f562e623c5c8c2b61c
Ghaida <alsabtighaida@gmail.com>
2026-04-29 04:05:33 +0000

    fixed logic

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,6 @@
 #include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
 /**
  * hash_table_set - adds a new node to hash table
  * @ht: the hash table
@@ -9,22 +11,42 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t new_node;
+	hash_node_t *new_node;
 	unsigned long int hash;
 	unsigned long int indx;
 
-	if (key == "")
+	if (key == NULL)
 		return (0);
-
+	
 	hash = hash_djb2(key);
-	
-	for (indx = 0; indx < ht->size; indx++)
+	indx = key_index(hash, ht->size);
+
+	/* update logic */
+	if (strcmp(ht->array[indx]->key, key) == 0)
 	{
-		if (ht->array[indx]->key == hash)
-			ht->array[indx]->next = new_node;
+		ht->array[indx]->value = value;
+		return (1);
 	}
-	
-	new_node->key = hash;
-	new_node->value = value;
-	return (1);
+
+	/* new node logic */
+	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+        
+        if (new_node == NULL)
+                return (0);
+
+	new_node->key = key;
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (ht->array[indx] == NULL)
+	{
+		ht->array[indx] = new_node;
+		return (1);
+	}
+	else 
+	{
+		new_node->next = *(ht->array[indx]);
+		ht->array[indx] = new_node;
+		return (1);
+	}
 }
