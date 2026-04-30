a81ed3d343f5c2876b763d3b41fb2e0649ed2e63
Ghaida <alsabtighaida@gmail.com>
2026-04-30 03:46:49 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,40 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 #include <string.h>
+/** 
+ * add_new_node - adds a new node to bucket
+ * @ht: hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 success, 0 otherwise
+ */
+int add_new_node(hash_table_t *ht, const char *key, const char *value)
+{
+        hash_node_t *new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+
+        if (new_node == NULL)
+                return (0);
+
+        new_node->key = strdup(key);
+        new_node->value = strdup(value);
+
+        if (new_node->key && new_node->value == NULL)
+                return (0);
+
+        new_node->next = NULL;
+        if (ht->array[indx] == NULL)
+        {
+                ht->array[indx] = new_node;
+                return (1);
+        }
+        else
+        {
+                new_node->next = ht->array[indx];
+                ht->array[indx] = new_node;
+                return (1);
+        }
+}
 /**
  * hash_table_set - adds a new node to hash table
  * @ht: the hash table
@@ -11,11 +45,10 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
 	hash_node_t *temp; /* for searching */
 	unsigned long int indx;
 
-	if (ht == NULL || key == NULL || value == NULL )
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
 	indx = key_index((const unsigned char *)key, ht->size);
@@ -33,30 +66,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-
 	/* new node logic */
-	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
-        
-        if (new_node == NULL)
-                return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key && new_node->value == NULL)
-		return (0);
-
-	new_node->next = NULL;
-
-	if (ht->array[indx] == NULL)
-	{
-		ht->array[indx] = new_node;
-		return (1);
-	}
-	else 
-	{
-		new_node->next = ht->array[indx];
-		ht->array[indx] = new_node;
-		return (1);
-	}
+	return (add_new_node(ht, key, value));
 }
