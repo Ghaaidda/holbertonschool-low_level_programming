1ab53e7e59415741a926b0c5011ff2e7c9ad3e49
Ghaida <alsabtighaida@gmail.com>
2026-05-09 12:50:48 +0300

    add task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* hash_table_print - prints hash table
+* @ht: hash table
+*
+* Return: void
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp; /* for searching */
+	unsigned long int indx = 0;
+
+	if (ht != NULL)
+    {
+        printf("{");
+        while (indx < ht->size)
+        {
+            printf("%s: %s", temp->key, temp->value);
+            temp = temp->next;
+        }
+        printf("}\n");
+}
