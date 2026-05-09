a4d34682f501476472235198a68dadf3caa004ac
Ghaida <alsabtighaida@gmail.com>
2026-05-09 21:42:04 +0300

    fixed logic

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -8,31 +8,27 @@
 * Return: void
 */
 void hash_table_print(const hash_table_t *ht)
-{
-	hash_node_t *last; /* to print the end */ 
+{ 
     hash_node_t *temp; /* for traversing */
 	unsigned long int indx;
+    int flag = 0; /* for printing comma */
 
-	if (ht != NULL)
+	if (ht == NULL)
+        return;
+    
+    printf("{");
+    for (indx = 0; indx < ht->size; indx++)
     {
-        printf("{");
-        for (indx = 0; indx < ht->size; indx++)
-        {
-            temp = ht->array[indx];
-
-            if (indx == (ht->size - 1))
-            {
-                    printf("\'%s\': \'%s\'", last->key, last->value);
-                    break;
-            }
-
-            while(temp)
-            {  
-                printf("\'%s\': \'%s\', ", temp->key, temp->value);
-                last = temp;
-                temp = temp->next;
-            }
+        temp = ht->array[indx];
+        while(temp)
+        {  
+            if (flag == 1)
+                printf(", ");
+                
+            printf("\'%s\': \'%s\', ", temp->key, temp->value);
+            flag = 1;
+            temp = temp->next;
         }
-        printf("}\n");
     }
+    printf("}\n");
 }    
